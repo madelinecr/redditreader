c72516c87ee47da48cbabeff10d0cdf4f243a0b2
Blaine Pace <blainepace@gmail.com>
2012-12-12 17:16:53 -0800

    Compound LinkList Layout
    Created a compound layout in link_row.xml and populated it with author, upvotes,
    and a placeholder image.
    
    Refs #3

--- src/info/bpace/redditreader/LinkAdapter.java
+++ src/info/bpace/redditreader/LinkAdapter.java
@@ -43,11 +43,15 @@ public class LinkAdapter extends BaseAdapter {
 		if (convertView == null)
 			vi = inflater.inflate(R.layout.link_row, null);
 		
-		TextView title = (TextView)vi.findViewById(R.id.title);
-		TextView score = (TextView)vi.findViewById(R.id.score);
+		TextView title        = (TextView)vi.findViewById(R.id.title);
+		TextView subtitle 	  = (TextView)vi.findViewById(R.id.author);
+		TextView commentcount = (TextView)vi.findViewById(R.id.commentcount);
+		TextView score        = (TextView)vi.findViewById(R.id.score);
 		
 		title.setText(data[position].getTitle());
-		score.setText(data[position].getScore());
+		subtitle.setText("by " + data[position].getAuthor());
+		commentcount.setText(data[position].getCommentCount() + " comments");
+		score.setText(data[position].getScore() + " points");
 		return vi;
 	}
 
--- src/info/bpace/redditreader/api/Link.java
+++ src/info/bpace/redditreader/api/Link.java
@@ -4,6 +4,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Link extends Thing {
+	
+	private String author;
+	private String commentCount;
 	private String title;
 	private String selftext;
 	private String url;
@@ -14,6 +17,8 @@ public class Link extends Thing {
 		super(jobject);
 		try {
 			JSONObject jdata = jobject.getJSONObject("data");
+			author = jdata.getString("author");
+			commentCount = jdata.getString("num_comments");
 			title = jdata.getString("title");
 			selftext = jdata.getString("selftext");
 			url = jdata.getString("url");
@@ -29,7 +34,14 @@ public class Link extends Thing {
 	public String toString() {
 		return title;
 	}
+	
+	public String getAuthor() {
+		return author;
+	}
 
+	public String getCommentCount() {
+		return commentCount;
+	}
 	public String getTitle() {
 		return title;
 	}
