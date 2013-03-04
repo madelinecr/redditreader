2f0e83a3221f420eedc38139217f987a611b825e
Blaine Pace <blainepace@gmail.com>
2013-03-03 22:24:02 -0800

    Added in-memory cache for images
    Added an LruCache for storing image thumbnails. This has improved performance on
    the Link ListView.

--- src/info/bpace/redditreader/LinkAdapter.java
+++ src/info/bpace/redditreader/LinkAdapter.java
@@ -1,11 +1,13 @@
 package info.bpace.redditreader;
 
-import info.bpace.redditreader.api.WebUtils;
 import info.bpace.redditreader.api.Link;
+import info.bpace.redditreader.api.WebUtils;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +20,24 @@ public class LinkAdapter extends BaseAdapter {
 	private Activity activity;
 	private LayoutInflater inflater;
 	private Link[] data;
+	
+	private LruCache<String, Bitmap> bitmapCache;
 
 	public LinkAdapter(Activity a, Link[] d) {
 		activity = a;
 		inflater = (LayoutInflater) activity
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		data = d;
+		
+		final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+		final int cacheSize = maxMemory / 8;
+		
+		bitmapCache = new LruCache<String, Bitmap>(cacheSize) {
+			@Override
+			protected int sizeOf(String key, Bitmap bitmap) {
+				return bitmap.getByteCount() / 1024;
+			}
+		};
 	}
 
 	@Override
@@ -51,13 +65,16 @@ public class LinkAdapter extends BaseAdapter {
 		TextView subtitle = (TextView) vi.findViewById(R.id.author);
 		TextView commentcount = (TextView) vi.findViewById(R.id.commentcount);
 		TextView score = (TextView) vi.findViewById(R.id.score);
+		ImageView thumbnail = (ImageView) vi.findViewById(R.id.thumbnail);
 
+		Log.v("REDDIT", "making new view from " + data[position].getUrl());
 		new ThumbTask(vi).execute(data[position].getUrl());
 
 		title.setText(data[position].getTitle());
 		subtitle.setText("by " + data[position].getAuthor());
 		commentcount.setText(data[position].getCommentCount() + " comments");
 		score.setText(data[position].getScore() + " points");
+		thumbnail.setImageBitmap(null);
 		return vi;
 	}
 
@@ -71,7 +88,19 @@ public class LinkAdapter extends BaseAdapter {
 
 		@Override
 		protected Bitmap doInBackground(String... url) {
-			return WebUtils.grabImage(url[0], true);
+			String imageName = url[0];
+			Log.v("REDDIT", "String: " + imageName);
+			Bitmap bmp = bitmapCache.get(imageName);
+			
+			// if the cache didn't return anything
+			if(bmp == null) {
+				bmp = WebUtils.grabImage(url[0], true);
+				// if we got a valid bitmap from the net
+				if(bmp != null) {
+					bitmapCache.put(imageName, bmp);
+				}
+			}
+			return bmp;
 		}
 
 		@Override
